8f2cdc6ce265b17bc6dc89261e1cc98e858d015d
Joel <joelguerrae1405@gmail.com>
2025-11-24 20:53:17 -0400

    feat: Implementacion de lectura de producto

---
+++ SGP.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using SGP.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGP.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<OperationResult> GetAllAsync();
+        Task<OperationResult> GetByIdAsync(int id);
+    }
+}
